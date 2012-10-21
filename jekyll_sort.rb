e79621041fcf32e0cc0012a1949222bcef5e1c2c
Kyle Paulsen <krazykylep@gmail.com>
2012-10-21 01:58:51 -0700

    Allowing posts to be sortable.

--- jekyll_sort.rb
+++ jekyll_sort.rb
@@ -1,10 +1,26 @@
 module Jekyll
-  
+
   class DataSorter < Jekyll::Generator
     safe true
     priority :lowest
-    
+
     def initialize(config)
+    end
+
+    def generate(site)
+      config = site.config
+      if !config['posts']
+        postData = []
+        site.posts.each { |post| 
+          postHash = post.data
+          postHash['url'] = post.url
+          postHash['content'] = post.content
+          postHash['date'] = post.date
+          postData.push(postHash)
+        }
+        config['posts'] = postData
+      end
+
       if !config['jekyll_sort']
         return
       end
@@ -17,20 +33,16 @@ module Jekyll
         else
           ans = data.sort
         end
-        
+
         case job['direction']
         when "down"
           ans.reverse!
         end
-        
+
         config[job['dest']] = ans
       end
     end
-    
-    def generate(site)
-      
-    end
-    
+
   end
-  
+
 end
